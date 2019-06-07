adc7aa9a4dc76ca8fe7034bc7210ba124663ee75
Rishabh Bhardwaj <rbjrishabh22@gmail.com>
2019-06-07 11:39:05 +0530

    First commit

---
+++ StringLibraryTest/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace StringLibraryTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestStartsWithUpper()
+        {
+            // Tests that we expect to return true.
+            string[] words = { "Alphabet", "Zebra", "ABC", "Demo", "ToOL" };
+            foreach (var word in words)
+            {
+                bool result = demoDllCreation.StringLibrary1.StartsWithUpper(word);
+                Assert.IsTrue(result,
+                       String.Format("Expected for '{0}': true; Actual: {1}",
+                                     word, result));
+            }
+        }
+
+        [TestMethod]
+        public void TestDoesNotStartWithUpper()
+        {
+            // Tests that we expect to return false.
+            string[] words = { "alphabet", "zebra", "abc", "monkey",
+                               "1234", ".", ";", " " };
+            foreach (var word in words)
+            {
+                bool result = demoDllCreation.StringLibrary1.StartsWithUpper(word);
+                Assert.IsFalse(result,
+                       String.Format("Expected for '{0}': false; Actual: {1}",
+                                     word, result));
+            }
+        }
+
+        [TestMethod]
+        public void DirectCallWithNullOrEmpty()
+        {
+            // Tests that we expect to return false.
+            string[] words = { string.Empty, null };
+            foreach (var word in words)
+            {
+                bool result = demoDllCreation.StringLibrary1.StartsWithUpper(word);
+                Assert.IsFalse(result,
+                       String.Format("Expected for '{0}': false; Actual: {1}",
+                                     word == null ? "<null>" : word, result));
+            }
+        }
+    }
+}
---
+++ demoDllCreation/StringLibrary1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace demoDllCreation
+{
+    public static class StringLibrary1
+    {
+        public static bool StartsWithUpper(this String str)
+        {
+            if (String.IsNullOrWhiteSpace(str))
+                return false;
+
+            Char ch = str[0];
+            return Char.IsUpper(ch);
+        }
+    }
+}
